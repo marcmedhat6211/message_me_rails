2cedd75d03158165363f6054faea1def965c4c1c
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-09-26 22:18:03 +0200

    creating user and messages models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Khaled", password: "123456")
+User.create(username: "Samy", password: "123456")
+User.create(username: "Marina", password: "123456")
+User.create(username: "Ramy", password: "123456")
+User.create(username: "Mina", password: "123456")
