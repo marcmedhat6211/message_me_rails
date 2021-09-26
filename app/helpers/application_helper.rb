42ef87d5e1c30eb5df8ac0facc351e01b0bae557
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-09-26 23:52:21 +0200

    authentication system and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def current_user
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "You are not allowed to perform that action"
+      redirect_to login_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # ROOT ROUTE
   root 'chatroom#index'
+
+  # SESSIONS ROUTES
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
